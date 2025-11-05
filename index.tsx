8130bcbab8d11f1fcb3185a2174b58a6d3fe4d24
Harshit Kowsik <harshitkowsik@gmail.com>
2025-11-05 08:10:46 +0530

    all update

--- index.tsx
+++ index.tsx
@@ -1,7 +1,5 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 
 const rootElement = document.getElementById('root');
@@ -12,8 +10,6 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <HelmetProvider>
       <App />
-    </HelmetProvider>
   </React.StrictMode>
 );
